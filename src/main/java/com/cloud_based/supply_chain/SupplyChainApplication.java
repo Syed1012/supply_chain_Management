5b678f8ecf4f898150cbded76e9bf43b273e9678
Syed1012 <syedmusaib66@gmail.com>
2024-10-25 17:53:09 +0530

    Made credentials safe, using .env and added dependency of .env to load and added code to setproperty of credentials in main file

--- src/main/java/com/cloud_based/supply_chain/SupplyChainApplication.java
+++ src/main/java/com/cloud_based/supply_chain/SupplyChainApplication.java
@@ -5,10 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class SupplyChainApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load(); // Load the .env file
+        System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
+        System.setProperty("MONGO_DB", dotenv.get("MONGO_DB"));
 		SpringApplication.run(SupplyChainApplication.class, args);
 	}
 
